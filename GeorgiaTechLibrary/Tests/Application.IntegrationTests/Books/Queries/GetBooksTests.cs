53fd665ccea34d95be1563efe624f7a51a69b00f
Radu Matusa <matusaradu@gmail.com>
2021-05-21 08:36:26 +0200

    Two integration tests and mapping unit test, TEST-2

--- GeorgiaTechLibrary/Tests/Application.IntegrationTests/Books/Queries/GetBooksTests.cs
+++ GeorgiaTechLibrary/Tests/Application.IntegrationTests/Books/Queries/GetBooksTests.cs
@@ -12,7 +12,7 @@ namespace Tests.Application.IntegrationTests.Books.Queries
     public class GetBooksTests : TestBase
     {
         [Test]
-        public async Task ShouldReturnAllBooks()
+        public async Task ShouldReturnOneBook()
         {
             await AddAsync(new Book
             {
@@ -31,6 +31,52 @@ namespace Tests.Application.IntegrationTests.Books.Queries
             var result = await SendAsync(query);
 
             Assert.AreEqual(1, result.Count());
+        }
+
+        [Test]
+        public async Task ShouldReturnAllBooks()
+        {
+            await AddAsync(new Book
+            {
+                ISBN = "355118969-6",
+                Title = "Red Riding Hood",
+                Year = 1985,
+                Language = "English",
+                Edition = 5,
+                Binding = "hardcover",
+                Can_Lend = true,
+                Is_Interesting = false
+            }) ;
+
+            await AddAsync(new Book
+            {
+                ISBN = "355118969-6",
+                Title = "Red Riding Hood",
+                Year = 1985,
+                Language = "English",
+                Edition = 6,
+                Binding = "hardcover",
+                Can_Lend = true,
+                Is_Interesting = false
+            }) ;
+
+            await AddAsync(new Book
+            {
+                ISBN = "355118969-6",
+                Title = "Red Riding Hood",
+                Year = 1985,
+                Language = "English",
+                Edition = 7,
+                Binding = "hardcover",
+                Can_Lend = true,
+                Is_Interesting = false
+            }) ;
+
+            var query = new GetBooksQuery();
+
+            var result = await SendAsync(query);
+
+            Assert.AreEqual(3, result.Count());
         }
     }
 }
